36e27b525a832442ecfd5897666fba192aeb93c0
JANAAS12 <janaabdalslam@gmail.com>
2025-01-16 16:18:14 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1;
+            Console.WriteLine("Enter String ");
+            str1 =Console.ReadLine();
+            Console.WriteLine(str1);
+            ///////////////////
+            int x = 5;
+            Console.WriteLine(x);
+            double y = 5.66;
+            Console.WriteLine(y);
+            string st = "Hello Wold";
+            Console.WriteLine(st);
+            char c = 'J';
+            Console.WriteLine(c);
+            bool b = true;
+            Console.WriteLine(b);
+            const int num= 12;
+            Console.WriteLine(num);
+
+        }
+    }
+}
